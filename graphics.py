910b31e606c02f5b432081992958fbd922b4c1cb
horstrs <horst.marcelo@gmail.com>
2025-10-16 14:47:12 -0300

    Added cell

--- graphics.py
+++ graphics.py
@@ -6,11 +6,11 @@ class Window:
         self.width = width
         self.height = height
         self.__root_widget = Tk()
-        self.__root_widget.title("YAAAAY")
-        self.canvas_widget = Canvas()
-        self.canvas_widget.pack()
-        self.__running = False
+        self.__root_widget.title("Maze Solver")
         self.__root_widget.protocol("WM_DELETE_WINDOW", self.close)
+        self.canvas_widget = Canvas(self.__root_widget, height=height, width=width)
+        self.canvas_widget.pack(fill=BOTH, expand=1)
+        self.__running = False
 
     def redraw(self):
         self.__root_widget.update_idletasks()
@@ -26,7 +26,7 @@ class Window:
         self.__running = False
 
     def draw_line(self, line, fill_color):
-        line.draw(self.__canvas_widget, fill_color)
+        line.draw(self.canvas_widget, fill_color)
 
 
 class Point:
@@ -49,3 +49,38 @@ class Line:
             fill=fill_color,
             width=2,
         )
+
+
+class Cell:
+    def __init__(self, window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = -1
+        self.__x2 = -1
+        self.__y1 = -1
+        self.__y2 = -1
+        self.__win = window
+
+    def draw(self, point_1, point_2):
+        self.__x1 = point_1.x
+        self.__y1 = point_1.y
+        self.__x2 = point_2.x
+        self.__y2 = point_2.y
+        bottom_left = Point(self.__x1, self.__y1)
+        bottom_right = Point(self.__x2, self.__y1)
+        top_left = Point(self.__x1, self.__y2)
+        top_right = Point(self.__x2, self.__y2)
+        if self.has_left_wall:
+            left_wall = Line(bottom_left, top_left)
+            self.__win.draw_line(left_wall, "black")
+        if self.has_top_wall:
+            left_wall = Line(top_left, top_right)
+            self.__win.draw_line(left_wall, "black")
+        if self.has_right_wall:
+            right_wall = Line(bottom_right, top_right)
+            self.__win.draw_line(right_wall, "black")
+        if self.has_bottom_wall:
+            bottom_wall = Line(bottom_left, bottom_right)
+            self.__win.draw_line(bottom_wall, "black")
--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-from graphics import Window, Point, Line
+from graphics import Window, Point, Line, Cell
 
 def main():
     win = Window(800, 600)
     point_1 = Point(5, 5)
-    point_2 = Point(5, 100)
-    point_3 = Point(100, 5)
-    line_1 = Line(point_1, point_2)
-    line_1.draw(win.canvas_widget, "black")
-    line_2 = Line(point_1, point_3)
-    line_2.draw(win.canvas_widget, "red")
-    line_3 = Line(point_2, point_3)
-    line_3.draw(win.canvas_widget, "gray")
+    point_2 = Point(40, 40)
+
+    point_3 = Point(40, 5)
+    point_4 = Point(75, 40)
+    
+    cell1 = Cell(win)
+    #cell1.draw(point_1, point_2)
+    cell2 = Cell(win)
+    #cell2.draw(point_3, point_4)
+
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(Point(50, 50), Point(100, 100))
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(Point(125, 125), Point(200, 200))
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(Point(225, 225), Point(250, 250))
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(Point(300, 300), Point(500, 500))
 
     win.wait_for_close()
 
