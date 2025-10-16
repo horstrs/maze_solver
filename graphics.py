554dc5d587473ae5a3ee635ef3aa3d4646e135cd
horstrs <horst.marcelo@gmail.com>
2025-10-16 14:19:17 -0300

    Added line drawign

--- graphics.py
+++ graphics.py
@@ -7,8 +7,8 @@ class Window:
         self.height = height
         self.__root_widget = Tk()
         self.__root_widget.title("YAAAAY")
-        self.__canvas_widget = Canvas()
-        self.__canvas_widget.pack()
+        self.canvas_widget = Canvas()
+        self.canvas_widget.pack()
         self.__running = False
         self.__root_widget.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -20,6 +20,32 @@ class Window:
         self.__running = True
         while self.__running:
             self.redraw()
+        print("window closed...")
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas_widget, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, point_1, point_2):
+        self.point_1 = point_1
+        self.point_2 = point_2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.point_1.x,
+            self.point_1.y,
+            self.point_2.x,
+            self.point_2.y,
+            fill=fill_color,
+            width=2,
+        )
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from window import Window
+from graphics import Window, Point, Line
 
 def main():
     win = Window(800, 600)
+    point_1 = Point(5, 5)
+    point_2 = Point(5, 100)
+    point_3 = Point(100, 5)
+    line_1 = Line(point_1, point_2)
+    line_1.draw(win.canvas_widget, "black")
+    line_2 = Line(point_1, point_3)
+    line_2.draw(win.canvas_widget, "red")
+    line_3 = Line(point_2, point_3)
+    line_3.draw(win.canvas_widget, "gray")
+
     win.wait_for_close()
 
 
